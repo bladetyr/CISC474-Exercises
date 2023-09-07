0e5255276c37f612c349ac290383decbe1a07cf0
bladetyr <bladetyr@udel.edu>
2023-09-07 08:20:59 -0400

    exercise 3 done for real this time

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ $( document ).ready(function() {
     console.log( "ready!" );
 });
 
-/*
-$( "#nameform" ).on( "submit", function( event ) {
-    alert("here");
-    var fname = document.getElementById('fname').value;
-    var lname = document.getElementById('lname').value;
-    //event.preventDefault();
-    return(fname.concat(lname));
-  });
-*/
-
 $(document).on("click", "#go", function( event ) {
     var fname = document.getElementById('fname').value;
     var lname = document.getElementById('lname').value;
